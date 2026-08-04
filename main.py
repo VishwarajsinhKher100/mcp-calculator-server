35d57f23759cccb666db4c76a6a948f70f8f270c
Vishwarajsinh Kher <vishwarajsinh.kher100@gmail.com>
2026-08-04 17:07:59 +0530

    config: change from http to see

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def divide(a: float, b: float) -> float:
     return a / b
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    mcp.run(transport="see", host="0.0.0.0", port=8000)
